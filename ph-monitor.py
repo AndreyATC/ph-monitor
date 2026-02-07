994c8cba43578763c361c97e646c3ffb79a9ef84
AndreyATC <AndreyATC@Gmail.com>
2026-02-07 08:38:01 +0200

    Refactor ph-monitor.py to improve readability

--- ph-monitor.py
+++ ph-monitor.py
@@ -169,7 +169,7 @@ if not df.empty:
     )
 
     # Налаштування осей
-    fig.update_yaxes(title_text="<b>pH</b>", color="#007acc", secondary_y=False, range=[7.0, 9.0])
+    fig.update_yaxes(title_text="<b>pH</b>", color="#007acc", secondary_y=False, range=[7.5, 8.5])
     fig.update_yaxes(title_text="<b>ORP (mV)</b>", color="#ff7f0e", secondary_y=True, range=[200, 400]) # Підлаштуйте діапазон під свій акваріум
 
     # Конфігурація для мобільних
@@ -217,4 +217,5 @@ if not df.empty:
 
 else:
     st.info("Даних не знайдено. Перевірте підключення або розширте діапазон дат.")
+
 
